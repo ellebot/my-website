734180c0c2523d06fd7bccf3945be54901e857da
ellebot <echau2008@gmail.com>
2019-01-26 22:13:18 -0800

    Added fading slideshow of gallery

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import galaxybun from './auto_galaxybun_small.jpg';
+import galaxybun2 from './galaxybun1_small.jpg';
 
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gallery: [galaxybun, galaxybun2],
+      imageIndex: 0,
+      };
+  }
+  
+   nextImage() {
+    let current = this.state.imageIndex + 1;
+    if (current >= this.state.gallery.length) {
+      current = 0;
+    }
+    this.setState({
+      gallery: this.state.gallery,
+      imageIndex: current,
+    });
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(() => this.nextImage(), 10000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  
   render() {
     return (     
       <div className="App">
-      <h1> Hello, and welcome to my Elle's art page! </h1>
-        <img src={galaxybun} className="w3-animate-fading" alt="galaxybun" />
+      <h1> Hello, and welcome to Elle's art page! </h1>
+      <div>{this.state.imageIndex}</div>
+      <img src={this.state.gallery[this.state.imageIndex]} className="w3-animate-fading" alt="image" />
       </div>
     );
   }
