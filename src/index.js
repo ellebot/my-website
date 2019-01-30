c2e5a56de3c81710a28824e57fe975852ebc8fbe
ellebot <echau2008@gmail.com>
2019-01-30 09:28:25 -0800

    testing with use of react-transition-group

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import galaxybun from './auto_galaxybun_small.jpg';
 import galaxybun2 from './galaxybun1_small.jpg';
+import { CSSTransitionGroup } from 'react-transition-group';
+/*
+function ImageCarousel(props) {
+  return (
+    <div>
+      <CSSTransitionGroup
+        transitionName="carousel"
+        transitionEnterTimeout={300}
+        transitionLeaveTimeout={300}>
+        <img src={props.imageSrc} key={props.imageSrc} />
+      </CSSTransitionGroup>
+    </div>
+  );
+}*/
+
+function RenderImage(props){
+  return (
+    <img src={props.nextImage} alt="current slide in gallery" />
+  );
+}
 
 class Hello extends React.Component {
   constructor(props) {
@@ -11,7 +31,9 @@ class Hello extends React.Component {
     this.state = {
       gallery: [galaxybun, galaxybun2],
       imageIndex: 0,
+      timer: 0,
       };
+    this.nextImage = this.nextImage.bind(this);
   }
   
    nextImage() {
@@ -22,11 +44,24 @@ class Hello extends React.Component {
     this.setState({
       gallery: this.state.gallery,
       imageIndex: current,
+      timer: this.state.timer,
+    });
+    //return <img src={this.state.gallery[this.state.imageIndex]} alt="current slide in gallery" onAnimationEnd={()=> return ({this.nextImage()});}/>
+    //this.forceUpdate();
+  }
+  
+  timer(){
+    let nextSecond = this.state.timer + 1;
+    this.setState({
+      gallery: this.state.gallery,
+      imageIndex: this.state.imageIndex,
+      timer: nextSecond,
     });
   }
   
   componentDidMount() {
-    this.interval = setInterval(() => this.nextImage(), 10000);
+    this.interval = setInterval(() => this.nextImage(), 3000);
+    this.interval = setInterval(() => this.timer(), 1000);
   }
   
   componentWillUnmount() {
@@ -37,8 +72,10 @@ class Hello extends React.Component {
     return (     
       <div className="App">
       <h1> Hello, and welcome to Elle's art page! </h1>
-      <div>{this.state.imageIndex}</div>
-      <img src={this.state.gallery[this.state.imageIndex]} className="w3-animate-fading" alt="image" />
+      <div>Bunny Image: {this.state.imageIndex + 1}</div>
+      <div>Seconds elapsed: {this.state.timer}</div>
+      //  {this.nextImage()}
+      <RenderImage nextImage={this.state.gallery[this.state.imageIndex]} />
       </div>
     );
   }
